270311e60680cb468bbbec427893b232857bd6fb
sicchio99 <giovannisicchio@gmail.com>
2024-12-13 16:33:20 +0100

    Experiment

---
+++ Experiment.py
@@ -0,0 +1,11 @@
+from Toprank import Toprank
+from Toprank2 import Toprank2
+
+if __name__ == '__main__':
+    # Step 1: import dei grafi
+    print("Start")
+    # Step 2: inizio ciclo (per ogni k da 10 a x)
+    # Step 2.1: inizio ciclo (per ogni grafo do -> n)
+    # Step 2.1.2: esegui Toprank per grafo g, salva risultato e durata
+    # Step 2.1.3: esegui
+
