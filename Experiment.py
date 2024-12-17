6975f4a4915f2a4ba4ad35e3bb8d0bbe3e9aa8b5
sicchio99 <giovannisicchio@gmail.com>
2024-12-17 16:36:22 +0100

    Experiment

--- Experiment.py
+++ Experiment.py
@@ -2,10 +2,14 @@ from Toprank import Toprank
 from Toprank2 import Toprank2
 
 if __name__ == '__main__':
+
+    k_value = [10, 20, 40, 80, 100, 200]
     # Step 1: import dei grafi
-    print("Start")
     # Step 2: inizio ciclo (per ogni k da 10 a x)
     # Step 2.1: inizio ciclo (per ogni grafo do -> n)
     # Step 2.1.2: esegui Toprank per grafo g, salva risultato e durata
-    # Step 2.1.3: esegui
+    # Step 2.1.3: esegui Toprank2 per grafo g, salva risultato e durata
+    # Step 2.1.4: confronta soluzione 1 e 2 con esatto e determina differenza, salva differenza
+    # Step 2.2: Grafico prestazioni
+    # Step 2.3: Grafico qualità del risultato
 
